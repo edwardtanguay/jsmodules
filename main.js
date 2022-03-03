c6e8668c992637b668bc493e3ac597528d507d99
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-03-03 11:02:44 +0100

    test and addParams

---
+++ main.js
@@ -0,0 +1,6 @@
+import { test } from './test.js';
+import { addParams } from './test.js';
+
+console.log(test);
+
+console.log(addParams('nnn'));
---
+++ test.js
@@ -0,0 +1,6 @@
+export const test = 'module works';
+
+// text --> (text)
+export const addParams = (text) => {
+	return '(' + text + ')';
+}
