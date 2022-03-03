4406859171d5fe96972cdc97d051e173e28dc028
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-03-03 13:12:23 +0100

    4 finished

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ customers.filter(m => m.address.city === 'London').forEach(m => console.log(`${m
 title('(3.1) get array of customers from London');
 console.log(customers.filter(m => m.address.city === 'London'));
 // const getCustomersFromLondon = () => {
-// 	const custs = []; 
+// 	const custs = [];
 // 	customers.forEach(m => {
 // 		if (m.address.city === 'London') {
 // 			custs.push(m);
@@ -34,4 +34,14 @@ console.log(customers.filter(m => m.address.city === 'London'));
 // }
 // console.log(getCustomersFromLondon());
 
+// (4) create function displayCustomerOrders(customerId) which returns a string the following information:
+// console.log(displayCustomerOrders('AROUT'));
+// returns Around the Horn (AROUT) has 14 orders
+title('create function displayCustomerOrders(customerId)');
+const displayCustomerOrders = (customerId) => {
+
+	return `${customers.find(m => m.customerID === customerId).companyName} (${customerId}) has ${orders.filter(m => m.customerID === customerId).length} orders`;
+};
+console.log(displayCustomerOrders('AROUT'));
+console.log(displayCustomerOrders('SPLIR'));
 
