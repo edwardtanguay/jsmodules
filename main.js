30c89ea6a3df7cf23ecede97b1ae5dec6d2e0fd6
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-03-03 11:10:27 +0100

    added orders

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 import { customers } from './customers.js';
+import { orders } from './orders.js';
 
 console.log(`There are ${customers.length} customers.`);
+console.log(`There are ${orders.length} orders.`);
