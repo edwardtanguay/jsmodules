e4ff4990c18d2865010529c1154aec0512688a11
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-03-03 11:19:32 +0100

    (1) finished

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ import { customers } from './customers.js';
 import { orders } from './orders.js';
 import { products } from './products.js';
 
-console.log(`There are ${customers.length} customers.`);
-console.log(`There are ${orders.length} orders.`);
-console.log(`There are ${products.length} products.`);
+// console.log(`There are ${customers.length} customers.`);
+// console.log(`There are ${orders.length} orders.`);
+// console.log(`There are ${products.length} products.`);
+
+// (1) display all company names of customers 
+// customers.filter(m => m.address.country === 'UK').forEach(customer => console.log(customer.companyName));
+customers.forEach(customer => console.log(customer.companyName));
+
