f433d5e4c4460f1043cc66ccb8c1e94cc00b0aac
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-03-03 11:32:01 +0100

    made title

--- main.js
+++ main.js
@@ -2,13 +2,22 @@ import { customers } from './customers.js';
 import { orders } from './orders.js';
 import { products } from './products.js';
 
+const title = (title) => {
+	console.log('');
+	console.log(`=== ${title} ================`);
+}
+
 // console.log(`There are ${customers.length} customers.`);
 // console.log(`There are ${orders.length} orders.`);
 // console.log(`There are ${products.length} products.`);
 
-// (1) display all company names of customers 
+title('(1) display all company names of customers');
 // customers.filter(m => m.address.country === 'UK').forEach(customer => console.log(customer.companyName));
 customers.forEach(customer => console.log(customer.companyName));
 
-// (2) display city - customerName
+title('(2) display city - customerName');
 customers.forEach(m => console.log(`${m.address.city} - ${m.companyName}`));
+
+title('(3) display city - customerName only for those from London');
+customers.filter(m => m.address.city === 'London').forEach(m => console.log(`${m.address.city} - ${m.companyName}`));
+
